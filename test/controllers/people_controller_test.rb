dd8404a99020dfe54e0ba08a64fe1910c21f16a0
Lenin Godinez <lenin@michelada.io>
2021-05-05 19:38:20 -0500

    stub people api request

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class PeopleControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    stub_request(:get, "https://api.salesloft.com/v2/people.json")
+      .to_return(status: 200, body: '{"data":[{"id": "1", "display_name": "test name", "email_address": "email@test.com"}]}', headers: {})
+  end
+
   test "should get index" do
     get people_url
 
