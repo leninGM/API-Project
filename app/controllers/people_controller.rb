665bdc3e20e36fc4080302ffa47d0471001128cd
Lenin Godinez <lenin@michelada.io>
2021-05-06 03:51:32 -0500

    Add functionality to show email Frequency Count

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -5,4 +5,15 @@ class PeopleController < ApplicationController
 
     @people = PeopleApi.new(page).all_people
   end
+
+  def email_chars_count
+    page = params["page"].to_i
+    page = page.zero? ? 1 : page
+
+    @people = PeopleApi.new(page).all_people
+
+    respond_to do |format|
+      format.js
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :people
+  resources :people do
+    get :email_chars_count, on: :collection
+  end
 
   root to: "people#index"
 end
