a9f1fd963b40150d8374ce0636f2fb79acc04901
Lenin Godinez <lenin@michelada.io>
2021-05-06 16:39:44 -0500

    fix tests

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class PeopleControllerTest < ActionDispatch::IntegrationTest
   setup do
     stub_request(:get, "https://api.salesloft.com/v2/people.json?include_paging_counts=true&page=1")
-      .to_return(status: 200, body: '{"data":[{"id": "1", "display_name": "test name", "email_address": "email@test.com"}]}', headers: {})
+      .to_return(status: 200, body: '{"data":[{"id": 1, "display_name": "test name", "email_address": "email@test.com"}]}', headers: {})
   end
 
   test "should get index" do
