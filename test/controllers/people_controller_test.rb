90dfd40c5ee7fbba5af799d9b895252aff201b12
Lenin Godinez <lenin@michelada.io>
2021-05-06 10:01:32 -0500

    Test email_chars_count path

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -2,13 +2,20 @@ require "test_helper"
 
 class PeopleControllerTest < ActionDispatch::IntegrationTest
   setup do
-    stub_request(:get, "https://api.salesloft.com/v2/people.json?page")
+    stub_request(:get, "https://api.salesloft.com/v2/people.json?page=1")
       .to_return(status: 200, body: '{"data":[{"id": "1", "display_name": "test name", "email_address": "email@test.com"}]}', headers: {})
   end
 
   test "should get index" do
-    get people_url
+    get people_path
 
     assert_response :success
+  end
+
+  test "should get email_chars_count" do
+    get email_chars_count_people_path, xhr: true
+
+    assert_response :success
+    assert_equal "text/javascript", @response.media_type
   end
 end
